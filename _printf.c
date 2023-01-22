c38c0ca5913511eb072b393d8cec0c83792ef100
Stephane ASSOCLE <mensste30@gmail.com>
2023-01-22 17:54:11 +0100

    printf function start

---
+++ _printf.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <unistd.h>
+
+/**
+ * _printf - print text
+ * @format: the text
+ *
+ * Return: the number of characters printed
+ */                                                                                                                  
+int _printf(const char *format, ...)
+{
+        va_list list;
+        int i;
+
+        va_start(list, format);
+
+        i = 0;
+        while(format[i])
+        {
+                if (format[i] == '%')
+                {
+                        if(format[i+1] == 'c')
+                        {
+                                write(1, va_arg(list, int *), 1);
+                                i += 2;
+                        }
+                }
+                else
+                {
+                        write(1, &format[i], 1);
+                        i++;
+                }
+        }
+        va_end(list);
+        return (0);
+}
